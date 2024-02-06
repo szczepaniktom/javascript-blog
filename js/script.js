370b0923c4ac5cd3a27a345de8077c70cce3e436
Tomasz Szczepanik <szczepaniktom@gmail.com>
2024-02-06 21:08:57 +0100

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log("link was clicked");
 
 
-    /* remove class 'active' from all article links  */
+    /* [done] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
 
 
-
-    /* remove class 'active' from all articles */
+    /* [done] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post.active');
 
     for(let activeArticle of activeArticles){
