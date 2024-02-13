4bd29e956f4a27397bb8da3e8e47a106dce4e5a4
Tomasz Szczepanik <szczepaniktom@gmail.com>
2024-02-13 21:32:02 +0100

    Add function generateTags (find all articles-get tags from data-tags attribute)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(){
 
@@ -75,3 +76,32 @@ const titleClickHandler = function(event){
 
 
 generateTitleLinks();
+
+
+function generateTags(){
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  /* START LOOP: for every article: */
+  for(let article of articles){
+    /* find tags wrapper */
+    const titleList = article.querySelector(optArticleTagsSelector);
+    /* make html variable with empty string */
+    let html = '';
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+  }
+  /* END LOOP: for every article: */
+}
+
+generateTags();
